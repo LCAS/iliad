900f2fcce0c93705a04d09e5a96397b8065f4b37
Manuel Fernandez-Carmona <mfernandezcarmona@lincoln.ac.uk>
2018-08-22 10:06:30 +0100

    Don't show debug info by default

--- twist_controller/src/tw_controller_node.cpp
+++ twist_controller/src/tw_controller_node.cpp
@@ -11,14 +11,13 @@ void cmdVelReceived(const geometry_msgs::Twist::ConstPtr& cmd_vel)
 {    
   v = cmd_vel->linear.x;
   rot = cmd_vel->angular.z;
-  ROS_INFO("[zombie@%d] vel command received (%2.2f, %2.2f)", __LINE__,v,rot);
+  ROS_DEBUG("[tw_controller_node@%d] vel command received (%2.2f, %2.2f)", __LINE__,v,rot);
   message_flags = SW_CAN_FLAG_OVERRIDE | SW_CAN_FLAG_DRIVE_ON | SW_CAN_FLAG_STEER_ON;
 
 }
 
 void debugMode()
 {    
-
 	if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
 	   ros::console::notifyLoggerLevelsChanged();
 	}
@@ -29,22 +28,28 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "tw_controller_node");
 
-  debugMode();
+  
 
   ros::NodeHandle n;
     
   double refresh_period_msecs= 20;
   double refresh_rate=1000.0/refresh_period_msecs; //Herzs
-  
+  bool isDebugMode;
+	
+  n.param<bool>("showDebug", isDebugMode, false);
+  if (debugMode){
+	  debugMode();
+  }
+	
   ros::Rate r(refresh_rate);
 
   ros::Subscriber sub = n.subscribe("z_cmd_vel", 1, cmdVelReceived);
-  ROS_INFO("[tw_controller_node@%d] ROS subscriber created ", __LINE__);
+  ROS_DEBUG("[tw_controller_node@%d] ROS subscriber created ", __LINE__);
   
   double _max_car_velocity_change;
   double _max_car_steer_angle_change;
   commandSendero command_sender( _max_car_velocity_change,  _max_car_steer_angle_change);
-  ROS_INFO("[tw_controller_node@%d] CAN command sender created ", __LINE__);
+  ROS_DEBUG("[tw_controller_node@%d] CAN command sender created ", __LINE__);
 
   while (n.ok())
   {
